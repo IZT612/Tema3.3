d8c8857df008771c05e3df9cb4d7a58e9656c577
IZT612 <ivan.zamora@iesnervion.es>
2025-01-26 23:49:37 +0100

    9 comentado y optimizado

--- Tema3.3/src/parte1/Ejercicio09.java
+++ Tema3.3/src/parte1/Ejercicio09.java
@@ -23,20 +23,26 @@ public class Ejercicio09 {
 
 		// Treemap donde guardamos los numeros
 		TreeMap<Integer, Integer> numeros = new TreeMap<>();
-
+		
+		// Treemap onde guardamos las estrellas
 		TreeMap<Integer, Integer> estrellas = new TreeMap<>();
 
+		// Do while para ir pidiendo euromillones
 		do {
 
+			// Bucle for para los numeros
 			for (int i = 0; i < 5; i++) {
 
+				// Pedimos el numero
 				System.out.println("Introduzca el " + (i + 1) + " número");
 				numero = sc.nextInt();
 
+				// Si no aparece, lo insertamos con valor 1
 				if (numeros.get(numero) == null) {
 
 					numeros.put(numero, 1);
 
+					// Si no, añadimos 1 al valor
 				} else {
 
 					numeros.put(numero, numeros.get(numero) + 1);
@@ -45,15 +51,19 @@ public class Ejercicio09 {
 
 			}
 
+			// Hacemos lo mismo para las estrellas
 			for (int i = 0; i < 2; i++) {
 
+				// Pedimos la estrella
 				System.out.println("Introduzca la " + (i + 1) + " estrella");
 				numero = sc.nextInt();
 
+				// Si no hay estrella, la guardamos con valor 1
 				if (estrellas.get(numero) == null) {
 
 					estrellas.put(numero, 1);
 
+					// Si no, añadimos 1 al valor
 				} else {
 
 					estrellas.put(numero, estrellas.get(numero) + 1);
@@ -62,31 +72,24 @@ public class Ejercicio09 {
 
 			}
 
-			numero = numeros.higherKey(numeroBajo);
-			numeroBajo = numero;
+			// Hacemos un for para mostrar todos los numeros
+			for (int i = 0; i < numeros.size(); i++) {
 
-			if (numeros.get(numero) == 1) {
-
-				System.out.print("El número " + numero + " se repite " + numeros.get(numero) + " vez");
-
-			} else {
-
-				System.out.print("El número " + numero + " se repite " + numeros.get(numero) + " veces");
-
-			}
-
-			for (int i = 0; i < numeros.size() - 1; i++) {
-
+				// Guardamos en "numero" la clave siguiente a "numeroBajo"
 				numero = numeros.higherKey(numeroBajo);
+				
+				// Guardamos el numero como el nuevo "numeroBajo"
 				numeroBajo = numero;
 
+				// Si está una sola vez mostramos el mensaje singular
 				if (numeros.get(numero) == 1) {
 
-					System.out.print(", el número " + numero + " se repite " + numeros.get(numero) + " vez");
+					System.out.print(", el número " + numero + " está " + numeros.get(numero) + " vez");
 
+					// Si no, en plural
 				} else {
 
-					System.out.print(", el número " + numero + " se repite " + numeros.get(numero) + " veces");
+					System.out.print(", el número " + numero + " está " + numeros.get(numero) + " veces");
 
 				}
 
@@ -94,6 +97,7 @@ public class Ejercicio09 {
 
 			System.out.print(".");
 
+			// Repetimos exactamente lo mismo para las estrellas:
 			System.out.println();
 			System.out.println("En cuanto a las estrellas: ");
 
@@ -101,28 +105,18 @@ public class Ejercicio09 {
 			numero = numeros.higherKey(numeroBajo);
 			numeroBajo = numero;
 
-			if (estrellas.get(numero) == 1) {
-
-				System.out.print("El número " + numero + " se repite " + estrellas.get(numero) + " vez");
-
-			} else {
-
-				System.out.print("El número " + numero + " se repite " + estrellas.get(numero) + " veces");
-
-			}
-
-			for (int i = 0; i < estrellas.size() - 1; i++) {
+			for (int i = 0; i < estrellas.size(); i++) {
 
 				numero = estrellas.higherKey(numeroBajo);
 				numeroBajo = numero;
 
 				if (estrellas.get(numero) == 1) {
 
-					System.out.print(", el número " + numero + " se repite " + estrellas.get(numero) + " vez");
+					System.out.print(", el número " + numero + " está " + estrellas.get(numero) + " vez");
 
 				} else {
 
-					System.out.print(", el número " + numero + " se repite " + estrellas.get(numero) + " veces");
+					System.out.print(", el número " + numero + " está " + estrellas.get(numero) + " veces");
 
 				}
 
@@ -134,24 +128,29 @@ public class Ejercicio09 {
 
 			numeroBajo = 0;
 
+			// Pedimos si quiere introducir otro
 			System.out.println("¿Quiere introducir otro euromillón?");
 			System.out.println("1. Sí");
 			System.out.println("2. No");
 			System.out.println();
 			numero = sc.nextInt();
 
+			// Si ha introducido 1, ponemos "introducirOtro" como true
 			if (numero == 1) {
 
 				introducirOtro = true;
 
+				// Si no, "introducirOtro" será false
 			} else {
 
 				introducirOtro = false;
 
 			}
 
+			// Se repite si "introducirOtro" es true
 		} while (introducirOtro);
 
+		// Cerramos el scanner
 		sc.close();
 
 	}
